51b0d9f8b8a2abf3a99d9d6f21b51ff1df10dc01
Felipe Arosemena <felipe@briteweb.com>
2018-01-15 23:58:13 -0800

    add inArray util method

--- utils.js
+++ utils.js
@@ -2,6 +2,15 @@
 module.exports.paramError = p => { throw new Error(`Missing parameter: ${p}`) }
 module.exports.envError = p => { throw new Error(`Missing .env variable: ${p}`) }
 
+
+/**
+ *
+ * @returns {boolean}
+ *
+ */
+
+module.exports.inArray = (item, array) => array.indexOf(item) > -1
+
 module.exports.moduleExists = name => {
   try { return require.resolve( name ) }
   catch( e ) { return false }
