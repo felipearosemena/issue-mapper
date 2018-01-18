2c20b1ef40b2f9e68d318aa189def96e37d22719
Felipe Arosemena <felipearosemena@gmail.com>
2018-01-18 01:29:10 -0800

    extract logging into util method

--- utils.js
+++ utils.js
@@ -1,4 +1,3 @@
-
 module.exports.paramError = p => { throw new Error(`Missing parameter: ${p}`) }
 
 module.exports.envError = p => { throw new Error(`Missing .env variable: ${p}`) }
@@ -9,3 +8,5 @@ module.exports.moduleExists = name => {
   try { return require.resolve( name ) }
   catch( e ) { return false }
 }
+
+module.exports.log = x => console.log(x)
